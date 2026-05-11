e108e1322c23208d2410dc123b9d64be16c3d6ce
Vignesh A S <asvicki2002@gmail.com>
2026-05-11 22:50:10 +0530

    updated favicon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -27,17 +27,17 @@ export default defineConfig({
         categories: ["productivity", "social"],
         icons: [
           {
-            src: "/icons/icon-192.png",
+            src: "/web-app-manifest-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512.png",
+            src: "/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512.png",
+            src: "/web-app-manifest-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
